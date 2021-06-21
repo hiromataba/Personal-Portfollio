b96e1e969857d469a01132a1a3434f69b8809b8a
Hiro Mataba <aganzemataba@gmail.com>
2021-06-21 20:46:42 +0200

    Fix linter errors

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-let menuBtn = document.querySelector(".burger");
-let menuOpen = document.querySelector("header nav ul");
-let menuClose = document.querySelector(".close-menu-btn button");
-let menuItems = document.querySelectorAll("header nav ul li a");
+const menuBtn = document.querySelector('.burger');
+const menuOpen = document.querySelector('header nav ul');
+const menuClose = document.querySelector('.close-menu-btn button');
+const menuItems = document.querySelectorAll('header nav ul li a');
 
-menuBtn.addEventListener("click", () => menuOpen.classList.add("mobileMenu"));
-menuClose.addEventListener("click", () =>
-  menuOpen.classList.remove("mobileMenu")
-);
-menuItems.forEach((item) =>
-  item.addEventListener("click", () => menuOpen.classList.remove("mobileMenu"))
-);
+menuBtn.addEventListener('click', () => menuOpen.classList.add('mobileMenu'));
+menuClose.addEventListener('click', () => menuOpen.classList.remove('mobileMenu'));
+menuItems.forEach((item) => item.addEventListener('click', () => menuOpen.classList.remove('mobileMenu')));
